47cace4818bb8a27cfd2209d103fdb027c38a99b
denis-tingaikin <denis.tingajkin@xored.com>
2022-10-11 03:24:01 +0300

    fix packet interdomain testing
    Signed-off-by: denis-tingaikin <denis.tingajkin@xored.com>

--- scripts/packet/k8s/start-master.sh
+++ scripts/packet/k8s/start-master.sh
@@ -12,7 +12,7 @@ mkdir -p "$HOME"/.kube
 sudo cp -f /etc/kubernetes/admin.conf "$HOME"/.kube/config
 sudo chown "$(id -u):$(id -g)" "$HOME"/.kube/config
 
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=192.168.0.0/16"
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
 
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
