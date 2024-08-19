918c022bf20083d56de4ae79c3ca18bc26af3237
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-08-20 10:50:02 +1100

    cleanup
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- scripts/gke/gke-start.sh
+++ scripts/gke/gke-start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
- # Get a specific GKE cluster version that matches NSM_KUBERNETES_VERSION
-K8S_VERSION=$(echo ${ vars.NSM_KUBERNETES_VERSION } | cut -d '.' -f 1,2 | cut -c 2-)
-GKE_CLUSTER_VERSION=$(gcloud container get-server-config --zone="$GKE_CLUSTER_ZONE" --format=json \
-    | jq '.channels[] | select (.channel=="REGULAR") | .validVersions[]' \
-    | grep -m 1 "$K8S_VERSION" | tr -d '"')
-if [ -z "$GKE_CLUSTER_VERSION"]; then
-    echo "GKE cluster version is not valid: $GKE_CLUSTER_VERSION"
-    exit 1
-fi
-
 gcloud components install gke-gcloud-auth-plugin
 gcloud components update
 time gcloud container clusters create "${GKE_CLUSTER_NAME}" \
