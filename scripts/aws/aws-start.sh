c0b08501afce73d554a1f8de187b6a92665e4a6f
NikitaSkrynnik <nikita.skrynnik@xored.com>
2024-08-20 11:21:33 +1100

    fix aws setup
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- scripts/aws/aws-start.sh
+++ scripts/aws/aws-start.sh
@@ -6,7 +6,8 @@ export IAM_NAME=ebs-csi-controller-sa
 
 apt-get update && apt-get -y install curl dnsutils
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/${AWS_K8S_VERSION}/bin/linux/amd64/kubectl
+echo $AWS_K8S_VERSION
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$AWS_K8S_VERSION/bin/linux/amd64/kubectl
 chmod +x kubectl
 mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
